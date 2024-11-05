0aec597c5bec4297cfb5f803265d3157ee67a839
milekboy <tijaniwahab25@gmail.com>
2024-11-05 18:02:09 +0100

    yes

--- app/api/NetworkInstance.js
+++ app/api/NetworkInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const NetworkInstance = () => {
   const networkInstance = axios.create({
-    baseURL: "//196.200.119.186:8070/",
+    baseURL: "http://196.200.119.186:8070/",
     // baseURL: "http://web-api/",
   });
 
